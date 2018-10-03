6cca99b26b7e07c61abf1b72304efc7241de51ef
tk14035 <petr.ljutik@teko.biz>
2018-10-03 11:45:28 +0300

    Первая фиксация. Каркас

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include "../inc/ver.h"
+
+int main()
+{
+  std::cout << "bulk version: "
+            << ver_major() << "."
+            << ver_minor() << "."
+            << ver_patch() << std::endl;
+
+  return 0;
+}
---
+++ test/test_main.cpp
@@ -0,0 +1,26 @@
+﻿#include "gtest/gtest.h"
+#include "../inc/ver.h"
+
+TEST(ver_test_case, ver_major_test) {
+  EXPECT_GE(ver_major(), 1);
+}
+
+TEST(ver_test_case, ver_minor_test) {
+  EXPECT_GE(ver_minor(), 1);
+}
+
+TEST(ver_test_case, ver_patch_test) {
+  EXPECT_GE(ver_patch(), 1);
+}
+
+TEST(matrix_test_case, default_ctor_test) {
+  using mat_t = mat::matrix<int, 0>;
+  mat_t mat;
+  EXPECT_EQ(mat.size(), 0);
+}
+
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
