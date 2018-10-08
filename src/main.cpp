213ab7fb025a412830576155ce7a827787d488ae
tk14035 <petr.ljutik@teko.biz>
2018-10-08 15:27:36 +0300

    Реализован вывод в файл.

--- src/main.cpp
+++ src/main.cpp
@@ -6,45 +6,70 @@
 #include <memory>
 #include <vector>
 #include <functional>
+#include <fstream>
+#include <ctime>
+
+using bulk_time_t = std::time_t;
+using bulk_t = std::vector<std::string>;
 
 class IStreamWriter {
   public:
-    virtual void write(const std::string&) = 0;
+    virtual ~IStreamWriter() = default;
+    virtual void write(const bulk_time_t&, const bulk_t&) = 0;
 };
 
 class ConsoleWriter : public IStreamWriter {
   public:
-    virtual void write(const std::string& str) final {
-      std::cout << "ConsoleWriter: " << str << std::endl;
+    ConsoleWriter() = default;
+    ~ConsoleWriter() = default;
+
+    virtual void write(const bulk_time_t&, const bulk_t& bulk) final {
+      std::cout << "bulk: ";
+      for(const auto &it : bulk) {
+        std::cout << it << (&it != &bulk.back() ? ", " : "\n");
+      }
     }
 };
 
 class FileWriter : public IStreamWriter {
   public:
-    virtual void write(const std::string& str) final {
-      std::cout << "FileWriter: " << str << std::endl;
+    FileWriter() = default;
+    ~FileWriter() = default;
+
+    virtual void write(const bulk_time_t& time, const bulk_t& bulk) final {
+      std::string file_name = "bulk" + std::to_string(time) + ".log";
+      std::fstream fs{file_name, std::ios::app};
+
+      fs << "bulk: ";
+      for(const auto& it : bulk) {
+        fs << it << (&it != &bulk.back() ? ", " : "\n");
+      }
+      fs.close();
     }
 };
 
 
 class IObservable {
   public:
+    virtual ~IObservable() = default;
     virtual void subscribe(std::unique_ptr<IStreamWriter> observer) = 0;
 };
 
-
 class BulkPool {
-
   public:
+    BulkPool() = default;
+    ~BulkPool() = default;
 
-    void push(std::string& str) {
-      pool_.emplace_back(str);
+    void push(const std::string& cmd) {
+      if(pool_.empty())
+        start_bulk_time_ = std::time(nullptr);
+      pool_.emplace_back(cmd);
     }
 
     template <typename F>
     void flush(F pred) {
-      for(auto it: pool_)
-        pred(it);
+      if(pred != nullptr)
+        pred(start_bulk_time_, pool_);
       pool_.clear();
     }
 
@@ -54,64 +79,31 @@ class BulkPool {
 
   private:
     std::vector<std::string> pool_{};
+    std::time_t start_bulk_time_{};
 };
 
-class OpenBlockInterpreter {
-  public:
-    void interpret(std::string& cmd) {
-    }
-
-  private:
-      size_t open_braces_{};
-};
-
-
-class CloseBlockInterpreter {
-  public:
-    void interpret(std::string& cmd) {
-    }
-
-  private:
-      size_t open_braces_{};
-};
-
-class CmdInterpreter {
-  public:
-    void interpret(std::string& cmd) {
-    }
-
-  private:
-      size_t open_braces_{};
-};
-
-class EofInterpreter {
-  public:
-    void interpret(std::string& cmd) {
-    }
-
-  private:
-      size_t open_braces_{};
-};
 
 class CmdProcessor : public IObservable  {
   public:
     CmdProcessor(size_t bulk_size) : bulk_size_(bulk_size) {
     }
 
+    virtual ~CmdProcessor() = default;
+
     void subscribe(std::unique_ptr<IStreamWriter> observer) final {
       stream_writers_.emplace_back(std::move(observer));
     }
 
-
-
-    void interpret(std::string& cmd) {
+    void process(const std::string& cmd) {
       if(cmd == "{") {
-        bulk_pool_.flush(publish);
+        if(open_braces_ == 0)
+          bulk_pool_.flush(publish);
         ++open_braces_;
       } else if(cmd == "}") {
         if(open_braces_) {
           --open_braces_;
-          bulk_pool_.flush(publish);
+          if(open_braces_ == 0)
+            bulk_pool_.flush(publish);
         }
       } else {
         bulk_pool_.push(cmd);
@@ -123,18 +115,18 @@ class CmdProcessor : public IObservable  {
 
   private:
 
-    std::function<void(const std::string& str)> publish = [&](const std::string& str) {
+    using publish_t = std::function<void(const std::time_t& bulk_time,
+                                         const std::vector<std::string>& cmds)>;
+    publish_t publish = [&](const std::time_t& bulk_time, const std::vector<std::string>& bulk) {
       for(auto& it: stream_writers_)
-        it->write(str);
+        it->write(bulk_time, bulk);
     };
 
 
     std::vector<std::unique_ptr<IStreamWriter>> stream_writers_{};
     BulkPool bulk_pool_;
-    bool is_block_{};
-    size_t bulk_size_{};
-
-    size_t open_braces_{};
+    size_t   bulk_size_{};
+    size_t   open_braces_{};
 };
 
 
@@ -154,11 +146,14 @@ int main()
   std::string openBrace = "{";
   std::string closeBrace = "}";
 
-  cmd_processor.interpret(cmd1);
-  cmd_processor.interpret(cmd2);
-  cmd_processor.interpret(openBrace);
-  cmd_processor.interpret(cmd3);
-  cmd_processor.interpret(closeBrace);
+  cmd_processor.process(cmd1);
+  cmd_processor.process(cmd2);
+  cmd_processor.process(openBrace);
+  cmd_processor.process(cmd3);
+  cmd_processor.process(cmd3);
+  cmd_processor.process(cmd3);
+  cmd_processor.process(cmd3);
+  cmd_processor.process(closeBrace);
 
   std::cout << "bulk version: "
             << ver_major() << "."
