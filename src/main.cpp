a87066f58ff5233a1cd106c5ab7018a325b46d65
tk14035 <petr.ljutik@teko.biz>
2018-10-09 16:48:47 +0300

    Реализован отдельный интерпретатор команд.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@
 #include <memory>
 #include <vector>
 #include <functional>
+#include <sstream>
 #include <fstream>
 #include <ctime>
+
 
 using bulk_time_t = std::time_t;
 using bulk_t = std::vector<std::string>;
@@ -63,7 +65,8 @@ class BulkPool {
     void push(const std::string& cmd) {
       if(pool_.empty())
         start_bulk_time_ = std::time(nullptr);
-      pool_.emplace_back(cmd);
+      if(!cmd.empty())
+        pool_.emplace_back(cmd);
     }
 
     template <typename F>
@@ -73,19 +76,46 @@ class BulkPool {
       pool_.clear();
     }
 
-    size_t size() {
-      return pool_.size();
-    }
-
   private:
     std::vector<std::string> pool_{};
     std::time_t start_bulk_time_{};
 };
 
+class CmdInterpreter {
+  public:
+    CmdInterpreter(size_t bulk_size) : bulk_size_(bulk_size) {
+    }
+
+    ~CmdInterpreter() = default;
+
+    bool interpret(const std::string& input, std::string& cmd) {
+      if(input == "{") {
+        if(tokens_++ == 0)
+          return true;
+      } else if(input == "}") {
+        if(tokens_ > 0)
+          if(--tokens_ == 0)
+            return true;
+      } else {
+        cmd = input;
+        if((tokens_ == 0) && (++size_ == bulk_size_)) {
+          size_ = 0;
+          return true;
+        }
+      }
+      return false;
+    }
+
+  private:
+
+    size_t bulk_size_{};
+    size_t size_{};
+    size_t tokens_{};
+};
 
 class CmdProcessor : public IObservable  {
   public:
-    CmdProcessor(size_t bulk_size) : bulk_size_(bulk_size) {
+    CmdProcessor(size_t bulk_size, std::istream& is = std::cin) : is_(is), interpreter_(bulk_size) {
     }
 
     virtual ~CmdProcessor() = default;
@@ -94,39 +124,28 @@ class CmdProcessor : public IObservable  {
       stream_writers_.emplace_back(std::move(observer));
     }
 
-    void process(const std::string& cmd) {
-      if(cmd == "{") {
-        if(open_braces_ == 0)
-          bulk_pool_.flush(publish);
-        ++open_braces_;
-      } else if(cmd == "}") {
-        if(open_braces_) {
-          --open_braces_;
-          if(open_braces_ == 0)
-            bulk_pool_.flush(publish);
-        }
-      } else {
-        bulk_pool_.push(cmd);
-        if((open_braces_ == 0) && (bulk_pool_.size() == bulk_size_)) {
-          bulk_pool_.flush(publish);
-        }
-      }
+    void process(const std::string& input) {
+      std::string cmd;
+      bool is_bulk_end = interpreter_.interpret(input, cmd);
+      bulk_pool_.push(cmd);
+      if(is_bulk_end)
+        bulk_pool_.flush(publish);
     }
 
   private:
 
-    using publish_t = std::function<void(const std::time_t& bulk_time,
-                                         const std::vector<std::string>& cmds)>;
-    publish_t publish = [&](const std::time_t& bulk_time, const std::vector<std::string>& bulk) {
+    using publish_t = std::function<void(const bulk_time_t& bulk_time,
+                                         const bulk_t& cmds)>;
+    publish_t publish = [&](const bulk_time_t& bulk_time, const bulk_t& bulk) {
       for(auto& it: stream_writers_)
         it->write(bulk_time, bulk);
     };
 
 
     std::vector<std::unique_ptr<IStreamWriter>> stream_writers_{};
+    std::istream& is_;
     BulkPool bulk_pool_;
-    size_t   bulk_size_{};
-    size_t   open_braces_{};
+    CmdInterpreter interpreter_;
 };
 
 
@@ -148,11 +167,19 @@ int main()
 
   cmd_processor.process(cmd1);
   cmd_processor.process(cmd2);
+  cmd_processor.process(cmd3);
+  cmd_processor.process(cmd3);
+  cmd_processor.process(openBrace);
+  cmd_processor.process(cmd1);
+  cmd_processor.process(openBrace);
+  cmd_processor.process(cmd2);
   cmd_processor.process(openBrace);
   cmd_processor.process(cmd3);
   cmd_processor.process(cmd3);
-  cmd_processor.process(cmd3);
-  cmd_processor.process(cmd3);
+  cmd_processor.process(closeBrace);
+  cmd_processor.process(cmd2);
+  cmd_processor.process(closeBrace);
+  cmd_processor.process(cmd1);
   cmd_processor.process(closeBrace);
 
   std::cout << "bulk version: "
