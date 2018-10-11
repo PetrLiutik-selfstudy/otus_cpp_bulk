a44e225dc0edb0ef1680d049c485324c6ecee8f0
tk14035 <petr.ljutik@teko.biz>
2018-10-11 16:17:26 +0300

    Начал добавлять тесты

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -13,13 +13,6 @@ TEST(ver_test_case, ver_patch_test) {
   EXPECT_GE(ver_patch(), 1);
 }
 
-TEST(matrix_test_case, default_ctor_test) {
-  using mat_t = mat::matrix<int, 0>;
-  mat_t mat;
-  EXPECT_EQ(mat.size(), 0);
-}
-
-
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
